11c8bed305abc101670ebe7c61bcdf7923b922a8
Josefina Nolting <josefinanolting@gmail.com>
2023-09-01 10:01:29 -0300

    Actualización del proyecto

--- proyecto_final/app1/urls.py
+++ proyecto_final/app1/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path('probandoTemplate/', views.probandoTemplates, name = "Probando Template"), 
+app_name = 'app1'  # Si estás usando namespaces, define el nombre de la aplicación
+
+urlpatterns = [
+    path('probandoTemplate/', views.probandoTemplates, name="Probando Template"), 
+    path('curso_list/', views.curso_list, name="curso_list"), 
+    path('curso_create/', views.curso_create, name="curso_create"), 
+
 ]
--- proyecto_final/app1/views.py
+++ proyecto_final/app1/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
+from .models import Curso
 
 #Create your views here
 
@@ -9,3 +10,18 @@ def probandoTemplates(request):
     context = {}  # Puedes agregar datos al contexto si es necesario
     documento = template.render(context)
     return HttpResponse(documento)
+
+def curso_list(request):
+    curso = Curso.objects.all()
+    return render(request, "curso_list.html", {"curso": curso})
+
+def curso_create(request):
+    if request.method == "POST":
+        nombre = request.POST.get("nombre")
+        numero_comision = request.POST.get("numero_comision")
+        curso = Curso(nombre=nombre, numero_comision=numero_comision)
+        curso.save()
+        return redirect("curso_list")
+    
+    return render(request, "curso_create.html")
+
